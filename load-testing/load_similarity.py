68917c35a39b8e3f8a85bd99efb78a69ff8533b1
dinateimouri <dinateimouri.dt@gmail.com>
2024-10-16 18:43:24 +0200

    add load testing for similarity measures

---
+++ load-testing/load_similarity.py
@@ -0,0 +1,11 @@
+from locust import task, FastHttpUser
+import locust.stats
+
+
+locust.stats.CONSOLE_STATS_INTERVAL_SEC = 60
+
+class MyUser(FastHttpUser):
+    @task
+    def load_similarity(self):
+        self.client.post(url="/sync-chat", json={"prompts": ["string1", "string2"], "similarity_measure": "cosine"})
+
