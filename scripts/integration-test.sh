c90c052400c97b8c3e87ee25f05569b8e3ccac4e
dinateimouri <dinateimouri.dt@gmail.com>
2024-10-16 11:26:42 +0200

    add an integration test and make it accept an arg

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+# This script is used to run integration tests on the API
+echo "You can also pass the URL of the API as an argument to this script"
+echo "The default URL is http://localhost:8080"
+echo "Example: ./scripts/integration-test.sh http://localhost:8000"
+echo "NOTE: There is no '/' at the end of the URL"
+echo ""
+echo "Running integration tests on the Yggdrasil API"
+echo "---------------------------------------------"
+if [ -z "$1" ]; then
+  URL=http://localhost:8080
+else
+  URL=$1
+fi
 
 # print function
 print_output () {
@@ -18,7 +31,7 @@ print_output () {
 test_func () {
   if [[ $3 -eq 'llm' ]]; then
     response=$(curl -X POST -s $1 -d "$4" -H 'Content-Type: application/json' -H 'accept: application/json')
-    if [[ $response == *"\*"* ]]; then
+    if [[ $response == *"$5"* ]]; then
       response_code=$3
     else
       response_code="fail"
@@ -34,13 +47,13 @@ test_func () {
   print_output $response_code "$2" $3
 }
 
-URL=http://localhost:8000
-
 test_func $URL/healthz "Get /healthz" 200
 
 test_func $URL/health "Get /health" 404
 
 test_func $URL/sync-chat "Post /sync-chat with complete data" 200 '{"prompts": ["string", "string"], "similarity_measure": "cosine"}'
+
+test_func $URL/sync-chat "Post /sync-chat with response from llm" "llm" '{"prompts": ["string", "string"], "similarity_measure": "cosine"}' "response"
 
 test_func $URL/sync-chat "Post /sync-chat with invalid similarity method" 422 '{"prompts": ["string", "string"], "similarity_measure": "invalid"}'
 
