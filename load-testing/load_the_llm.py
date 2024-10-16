ff829d67f8d6a89f45f0b27a11acfe12d39943f2
dinateimouri <dinateimouri.dt@gmail.com>
2024-10-16 18:43:07 +0200

    add load-testing for the llm

---
+++ load-testing/load_the_llm.py
@@ -0,0 +1,11 @@
+from locust import task, FastHttpUser
+import locust.stats
+
+
+locust.stats.CONSOLE_STATS_INTERVAL_SEC = 60
+
+class EndUser(FastHttpUser):
+    @task
+    def load_the_llm(self):
+        self.client.post(url="/sync-chat", json={"prompts": ["string", "string"], "similarity_measure": "cosine"})
+
