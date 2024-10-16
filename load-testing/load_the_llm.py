db1a24a2dd9d46f97fbd4e1d727ec2f88f54b89d
dinateimouri <dinateimouri.dt@gmail.com>
2024-10-16 19:50:07 +0200

    fix pre-commit suggestions

--- load-testing/load_the_llm.py
+++ load-testing/load_the_llm.py
@@ -4,8 +4,14 @@ import locust.stats
 
 locust.stats.CONSOLE_STATS_INTERVAL_SEC = 60
 
+
 class EndUser(FastHttpUser):
     @task
     def load_the_llm(self):
-        self.client.post(url="/sync-chat", json={"prompts": ["string", "string"], "similarity_measure": "cosine"})
-
+        self.client.post(
+            url="/sync-chat",
+            json={
+                "prompts": ["string", "string"],
+                "similarity_measure": "cosine",
+            },
+        )
